e615c72b61de6e78cfd558c8dc664a98b595a6a7
Nate <nate.kiprotich@gmail.com>
2021-06-20 09:55:09 +0300

    Showing the amount of time to complete execution

--- data_processing.py
+++ data_processing.py
@@ -3,6 +3,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
 import json
 import pandas as pd
+import time
+
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -29,7 +31,9 @@ for i in show_resp.json():
 '''
 
 if __name__ == "__main__":
-    
+        
+    start_time = time.time()
+    print(start_time)
     base_url = 'https://hacker-news.firebaseio.com/v0/'
     item_base_url = base_url + 'item/'
     
@@ -51,17 +55,25 @@ if __name__ == "__main__":
     
     df = pd.DataFrame()
 
-    for i in response.json():
+    returned_items = response.json()
+    
+    item_count = len(returned_items)
+    cnt = 0
+
+    print('{} items to be stored'.format(item_count))
+    for i in returned_items:
+        
         item_url = item_base_url + str(i) + '.json'
         item_response = requests.get(item_url)
         
         df = df.append(item_response.json(), ignore_index=True)
-
+        cnt += 1
+        print(str(item_count - cnt) + ' items remaining')
         # Kids to be in separate data frame and they should call item url
 
 #    spark_df = spark.createDataFrame(df)
-    print(df.columns)
-    print(df['kids'])
+   
+    print('Time taken is : ' + str(time.time() - start_time))
 
-    for i in df['kids']:
-        print(i)
+
+
